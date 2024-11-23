d1377bd418d51ecb2e56b5705c0c75e1538e5054
utkarsh-1912 <rashmi.2.10.75@gmail.com>
2024-11-23 14:50:08 +0530

    v0.0.1

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
 require('dotenv').config();
 
 const notesRoutes = require('./routes/notes');
@@ -19,12 +20,23 @@ app.use('/api', notesRoutes);
 // MongoDB Connection
 const startServer = async () => {
   try {
+    // Connect to MongoDB
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log('Connected to MongoDB Atlas');
-    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+    // Serve static files if on production (OnRender)
+    if (process.env.NODE_ENV === 'production') {
+      app.use(express.static(path.join(__dirname, 'frontend/build')));
+      app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+      });
+    }
+
+    // Start server
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   } catch (error) {
     console.error('Error connecting to database:', error.message);
     process.exit(1);
